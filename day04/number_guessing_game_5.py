c7eeb5cbaaeffdc6748317c99d627d0c1a4adeba
MayaSalo <93050581+MayaSalo@users.noreply.github.com>
2025-05-14 14:06:56 +0300

    Create number_guessing_game_5.py

---
+++ day04/number_guessing_game_5.py
@@ -0,0 +1,41 @@
+import random
+
+random_number = random.randint(1, 20)
+
+debug_mode = False
+move_mode = False
+
+while True:
+
+  if debug_mode:
+    print("Debug - The number is: ",random_number)
+
+  if move_mode:
+    addition = random.randint(-2, 2)
+    random_number = random_number+addition
+    
+  user_input = input("Enter your guess [x for exit, s for help, d for debug]: ")
+  
+  if user_input == 'x':
+    print("Bye Bye :( ")
+    exit()
+  elif user_input == 's':
+    print("The number is: ",random_number)
+    continue
+  elif user_input == 'd':
+    debug_mode = not debug_mode
+    continue  
+  elif user_input == 'm':
+    move_mode = not move_mode
+    continue    
+
+  guess = int(user_input)
+  
+  if guess == random_number:
+    print("CORRECT! you guessed the number!")
+    break
+  
+  if guess > random_number:
+    print("The number is smaller than your guess")
+  else:
+    print("The number is bigger than your guess")
