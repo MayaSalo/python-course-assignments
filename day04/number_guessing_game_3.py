d9b04c25f382edcde1c9389374203f16ce231a0a
MayaSalo <93050581+MayaSalo@users.noreply.github.com>
2025-05-14 13:53:32 +0300

    Create number_guessing_game_3.py

---
+++ day04/number_guessing_game_3.py
@@ -0,0 +1,24 @@
+import random
+
+random_number = random.randint(1, 20)
+
+while True:
+  user_input = input("Guess a whole number between 1 to 20 [x to exit and s for help]: ")
+  
+  if user_input == 'x':
+    print("Bye Bye :( ")
+    exit()
+  elif user_input == 's':
+    print("The number is: ",random_number)
+    continue
+
+  guess = int(user_input)
+  
+  if guess == random_number:
+    print("CORRECT! you guessed the number!")
+    break
+  
+  if guess > random_number:
+    print("The number is smaller than your guess")
+  else:
+    print("The number is bigger than your guess")
