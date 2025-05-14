751a10bd66beae12e47126b7147433f72105dd1b
MayaSalo <93050581+MayaSalo@users.noreply.github.com>
2025-05-14 14:17:56 +0300

    Update number_guessing_game_6.py

--- day04/number_guessing_game_6.py
+++ day04/number_guessing_game_6.py
@@ -1,53 +1,54 @@
 import random
 
-random_number = random.randint(1, 20)
+
 
 debug_mode = False
 move_mode = False
-new = False
 
 while True:
+  print("Game start")
+  random_number = random.randint(1, 20)
 
-  if new:
-    random_number = random.randint(1, 20)
-
-  if debug_mode:
-    print("Debug - The number is: ",random_number)
-
-  if move_mode:
-    addition = random.randint(-2, 2)
-    random_number = random_number+addition
+  while True:
+  
+    if new:
+      random_number = random.randint(1, 20)
+  
+    if debug_mode:
+      print("Debug - The number is: ",random_number)
+  
+    if move_mode:
+      addition = random.randint(-2, 2)
+      random_number = random_number+addition
+      
+    user_input = input("Enter your guess [x for exit, s for help, d for debug, m for move, n for new]: ")
     
-  user_input = input("Enter your guess [x for exit, s for help, d for debug, m for move, n for new]: ")
+    if user_input == 'x':
+      print("Bye Bye :( ")
+      exit()
+    elif user_input == 's':
+      print("The number is: ",random_number)
+      continue
+    elif user_input == 'd':
+      debug_mode = not debug_mode
+      continue  
+    elif user_input == 'm':
+      move_mode = not move_mode
+      continue    
+    elif user_input == 'n':
+      print("Get ready for a new game!")
+      break     
   
-  if user_input == 'x':
-    print("Bye Bye :( ")
-    exit()
-  elif user_input == 's':
-    print("The number is: ",random_number)
-    continue
-  elif user_input == 'd':
-    debug_mode = not debug_mode
-    continue  
-  elif user_input == 'm':
-    move_mode = not move_mode
-    continue    
-  elif user_input == 'n':
-    print("ready for a new game?")
-    new = not new
-    continue     
-
-  guess = int(user_input)
+    guess = int(user_input)
+    
+    if guess == random_number:
+      print("CORRECT! you guessed the number!")
+      print("Get ready for a new game!")
+      break
+    
+    if guess > random_number:
+      print("The number is smaller than your guess")
+    else:
+      print("The number is bigger than your guess")
   
-  if guess == random_number:
-    print("CORRECT! you guessed the number!")
-    print("ready for a new game?")
-    new = not new
-    continue
   
-  if guess > random_number:
-    print("The number is smaller than your guess")
-  else:
-    print("The number is bigger than your guess")
-
-
