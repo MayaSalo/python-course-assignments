6ae7f162afaebc71df13164231dbb190f5a3f141
MayaSalo <93050581+MayaSalo@users.noreply.github.com>
2025-05-14 13:29:44 +0300

    Create number_guessing_game_1.py

---
+++ day04/number_guessing_game_1.py
@@ -0,0 +1,15 @@
+import random
+
+random_number = random.randint(1, 20)
+
+guess = int(input("Guess a whole number between 1 to 20: "))
+
+while guess != random_number:
+  if guess > random_number:
+    print("The number is smaller than your guess")
+    guess = int(input("Guess a different whole number between 1 to 20: "))
+  elif guess < random_number:
+    print("The number is bigger than your guess")
+    guess = int(input("Guess a different whole number between 1 to 20: "))
+  else:
+    print("CORRECT! you guessed the number!")
