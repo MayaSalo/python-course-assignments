73fe2e87b6c648e54ebb9240b320f15a8a31d537
MayaSalo <93050581+MayaSalo@users.noreply.github.com>
2025-05-21 11:27:38 +0300

    Update dna_sequencing.py

--- day05/dna_sequencing.py
+++ day05/dna_sequencing.py
@@ -7,6 +7,5 @@ dna_sequence = sys.argv[1]
 dna_sequence_split = dna_sequence.split('X')
 print("splited seq: ",dna_sequence_split)
 
-dna_sequence_split_sort = dna_sequence_split.sort()
-print("sorted seq: ",dna_sequence_split_sort)
+print("sorted seq: ",dna_sequence_split.sort())
 
