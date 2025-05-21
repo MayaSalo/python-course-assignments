a850e0d1fb32bf680aca758ce96498e39082fa0b
MayaSalo <93050581+MayaSalo@users.noreply.github.com>
2025-05-21 11:41:28 +0300

    Update dna_sequencing.py

--- day05/dna_sequencing.py
+++ day05/dna_sequencing.py
@@ -3,6 +3,9 @@ import sys
 dna_sequence = sys.argv[1]
 
 dna_sequence_split = dna_sequence.split('X')
-dna_sequence_split.sort()
+
+dna_sequence_split = [segment for segment in dna_sequence_split if segment]
+
+dna_sequence_split.sort(key=len)
 
 print("fixed seq: ",dna_sequence_split)
