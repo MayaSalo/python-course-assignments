cdfa70bbd974cd6fcf645b95b21a28b7af32fe33
MayaSalo <93050581+MayaSalo@users.noreply.github.com>
2025-05-21 11:37:23 +0300

    Update dna_sequencing.py

--- day05/dna_sequencing.py
+++ day05/dna_sequencing.py
@@ -1,11 +1,8 @@
 import sys
 
-bases = ["A","C","T","G"]
-
 dna_sequence = sys.argv[1]
 
 dna_sequence_split = dna_sequence.split('X')
-print("splited seq: ",dna_sequence_split)
+dna_sequence_split.sort()
 
-print("sorted seq: ",dna_sequence_split.sort())
-
+print("fixed seq: ",dna_sequence_split)
