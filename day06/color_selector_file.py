47477ffbd81c501358eee701be1cf8de321fb246
MayaSalo <93050581+MayaSalo@users.noreply.github.com>
2025-06-04 08:42:56 +0300

    Create color_selector_file.py

---
+++ day06/color_selector_file.py
@@ -0,0 +1,10 @@
+with open('colors.txt') as fh:
+  colors = []
+  for line in fh:
+    colors.append(line.rstrip("\n"))
+    
+for i in range(len(colors)):
+  print("{}) {}".format(i, colors[i]))
+  
+selected = int(input("Select color: "))
+print("The selected color is: ", colors[selected])
