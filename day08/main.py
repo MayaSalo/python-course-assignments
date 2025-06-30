feb0424e73669391ef5e86cfee1a52bf9a6059b6
MayaSalo <93050581+MayaSalo@users.noreply.github.com>
2025-06-30 11:47:27 +0300

    Create main.py

---
+++ day08/main.py
@@ -0,0 +1,7 @@
+from color_utils import read_colors_from_file, display_colors, get_user_selection
+
+def main():
+    colors = read_colors_from_file("colors.txt")
+    display_colors(colors)
+    selected_color = get_user_selection(colors)
+    print("The selected color is:", selected_color)
